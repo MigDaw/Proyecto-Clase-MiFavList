3e3f549af36bfece51fe5ddb33cf9136aca1a151
Miguel <migdaw25@gmail.com>
2025-04-21 12:22:10 +0200

    logos en assets, borro cosas que no necesito y creo componente HeaderComp

---
+++ frontend/src/components/HeaderComp.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <img src="../assets/Logo 1.png" alt="">
+        <a href="#">Iniciar Sesión/Registrarse</a>
+    </header>
+</template>
+
+<style scoped>
+    img{
+        width: 150px;
+    }
+
+    a{
+        text-decoration: none;
+        color: white;
+        border: solid 1px white;
+        padding: 7px;
+        border-radius: 1rem;
+        font-weight: bold;
+    }
+</style>
