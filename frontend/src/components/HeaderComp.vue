d1ed3965d1beda427480841beb97d1bc0ec37f7c
Miguel <migdaw25@gmail.com>
2025-05-27 11:59:19 +0200

    front: cambio estructura header con icones de RRSS

--- frontend/src/components/HeaderComp.vue
+++ frontend/src/components/HeaderComp.vue
@@ -1,10 +1,56 @@
+<template>
+  <header>
+    <img src="../assets/Logo 1.png" alt="logo" />
+
+    <div class="socials">
+      <a href="/contacto" target="_blank"
+        ><img :src="logoGmail" alt="Icono gmail" title="Contacto por correo electrónico"
+      /></a>
+      <a href="https://github.com/MigDaw" target="_blank"
+        ><img :src="logoGithub" alt="Logo de GitHub" title="Perfil de GitHub"
+      /></a>
+      <a href="https://instagram.com/" target="_blank"
+        ><img :src="logoInstagram" alt="Logo de Instagram" title="Perfil de Instagram"
+      /></a>
+      <a href="https://x.com/?lang=es" target="_blank"
+        ><img :src="logoX" alt="Logo de X" title="Perfil de X"
+      /></a>
+    </div>
+
+    <div v-if="userStore" class="user-wrapper">
+      <div class="user-display" @click="toggleMenu">
+        <img
+          :src="
+            userStore?.profilePic
+              ? `http://localhost:8080${userStore.profilePic}`
+              : imagenGenerica
+          "
+          alt="Foto de perfil"
+          class="img-user"
+        />
+        {{ userStore.username }}
+        <span class="chevron">{{ menuOpen ? "▲" : "▼" }}</span>
+      </div>
+
+      <div v-if="menuOpen" class="dropdown">
+        <router-link to="/perfil">Ver perfil</router-link>
+        <a @click="logout">Cerrar sesión</a>
+      </div>
+    </div>
+  </header>
+</template>
+
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import api from '../axios';
-import { useRouter } from 'vue-router';
-import { userStore } from '../stores/authStore';
-import imagenGenerica from '../assets/Imagen-generica.png'
-import { useToast } from 'vue-toastification';
+import { ref, onMounted } from "vue";
+import api from "../axios";
+import { useRouter } from "vue-router";
+import { userStore } from "../stores/authStore";
+import imagenGenerica from "../assets/Imagen-generica.png";
+import { useToast } from "vue-toastification";
+import logoGithub from "../assets/logo-github.svg";
+import logoGmail from "../assets/logo-gmail.svg";
+import logoInstagram from "../assets/logo-instagram.svg";
+import logoX from "../assets/logo-x.svg";
 
 const toast = useToast();
 
@@ -16,15 +62,15 @@ const toggleMenu = () => {
 };
 
 const logout = async () => {
-  await api.post('/api/logout', {}, { withCredentials: true });
+  await api.post("/api/logout", {}, { withCredentials: true });
   userStore.value = null;
-  router.push('/');
-  toast.success('Has cerrado sesión');
+  router.push("/");
+  toast.success("Has cerrado sesión");
 };
 
 onMounted(async () => {
   try {
-    const response = await api.get('/api/me', {
+    const response = await api.get("/api/me", {
       withCredentials: true,
     });
     userStore.value = response.data;
@@ -33,25 +79,6 @@ onMounted(async () => {
   }
 });
 </script>
-
-<template>
-  <header>
-    <img src="../assets/Logo 1.png" alt="logo" />
-
-    <div v-if="userStore" class="user-wrapper">
-      <div class="user-display" @click="toggleMenu">
-        <img :src="userStore?.profilePic ? `http://localhost:8080${userStore.profilePic}` : imagenGenerica" alt="Foto de perfil" class="img-user" />
-        {{ userStore.username }}
-        <span class="chevron">{{ menuOpen ? '▲' : '▼' }}</span>
-      </div>
-
-      <div v-if="menuOpen" class="dropdown">
-        <router-link to="/perfil">Ver perfil</router-link>
-        <a @click="logout">Cerrar sesión</a>
-      </div>
-    </div>
-  </header>
-</template>
 
 <style scoped>
 header {
@@ -62,7 +89,27 @@ header {
   padding: 10px 20px;
 }
 
-.img-user{
+.socials {
+  display: flex;
+  gap: 18px;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+}
+
+.socials img {
+  width: 28px;
+  height: 28px;
+  filter: brightness(0) invert(1); /* Blanco sobre fondo oscuro */
+  transition: transform 0.2s;
+  
+}
+
+.socials img:hover {
+  transform: scale(1.15);
+}
+
+.img-user {
   width: 40px;
 }
 
@@ -84,6 +131,9 @@ img {
   display: flex;
   align-items: center;
   gap: 6px;
+  box-shadow: 10px 10px 24px 0px rgba(0,0,0,1);
+  -webkit-box-shadow: 10px 10px 24px 0px rgba(0,0,0,1);
+  -moz-box-shadow: 10px 10px 24px 0px rgba(0,0,0,1);
 }
 
 .chevron {
